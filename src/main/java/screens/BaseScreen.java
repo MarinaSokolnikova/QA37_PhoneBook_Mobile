909f9e5ca023ed624e2460347df702913885b468
MarinaSokolnikova <sokolnikovam92@gmail.com>
2023-05-13 15:19:31 +0300

    login success

--- src/main/java/screens/BaseScreen.java
+++ src/main/java/screens/BaseScreen.java
@@ -4,7 +4,10 @@ import config.AppiumConfig;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidElement;
 import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class BaseScreen {
 
@@ -13,5 +16,21 @@ public class BaseScreen {
     public BaseScreen(AppiumDriver<AndroidElement> driver) {
         this.driver = driver;
         PageFactory.initElements(new AppiumFieldDecorator(driver), this);
+    }
+
+    public void type(AndroidElement element, String text){
+        if (text!=null){
+            element.click();
+            element.clear();
+            element.sendKeys(text);
+        }
+    }
+
+    public boolean isShouldHave(AndroidElement element, String text, int time){
+        return new WebDriverWait(driver, time).until(ExpectedConditions.textToBePresentInElement(element, text));
+    }
+
+    public void should(AndroidElement element, int time) {
+        new WebDriverWait(driver, time).until(ExpectedConditions.visibilityOf(element));
     }
 }
